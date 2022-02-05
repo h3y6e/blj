d6b9d7f3ddfb0374f314f7e2803c58b1a758e40c
5ebec <hash.a5ebec@gmail.com>
2022-02-05 09:20:53 +0900

    update script

--- autologin_mmlan.sh
+++ autologin_mmlan.sh
@@ -2,13 +2,16 @@
 
 set -Eeuo pipefail
 
-cd `dirname $0`
+cd "$(dirname "$0")"
 
-eval $(egrep -v '^#' .env | xargs)
+eval "$(grep -E -v '^#' .env | xargs)"
 
-SSID=`/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I | awk '/ SSID:/ {print $2}'`
+SSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I | awk '/ SSID:/ {print $2}')
+echo "$SSID"
 
-if [ "${SSID}" = "mm2020" ] || [ "${SSID}" = "mm2010" ]; then
-    LOGIN_URL=`curl -w "%{redirect_url}" -s -o /dev/null http://www.naist.jp`
-    curl ${LOGIN_URL} -d "RequestType=Login" -d "UE-Username=${UE_USERNAME}" -d "UE-Password=${UE_PASSWORD}"
+if [ "$SSID" = "mm2020" ] || [ "$SSID" = "mm2010" ]; then
+    LOGIN_URL=$(curl -w "%{redirect_url}" -s -o /dev/null http://www.naist.jp)
+    if [[ "$LOGIN_URL" == "https://ruckus-sz.naist.jp:9998/SubscriberPortal/login"* ]]; then
+        curl "$LOGIN_URL" -d "RequestType=Login" -d "UE-Username=$UE_USERNAME" -d "UE-Password=$UE_PASSWORD"
+    fi
 fi
